ca4b56acbab3e09798582db6dc912d4d1cde4742
linhpd <linhpd@soict.hust.github.com>
2020-12-19 16:56:25 +0700

    Revert "view"
    This reverts commit 06136a74e0c98a9586bfd2ffb5fb67e73aa879d4.

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -30,7 +30,49 @@
                                     <span id="card-element" class="badge badge-danger ml-0" style='border-radius:50%'>
                                         <?php Session::get('user_cart') ?>
                                     </span></a>
-   
+    <body>
+
+        <div class="header">
+            <div class="top-header">
+                <div class="container">
+                    <div class="menu">
+                        <ul class="">
+                        <li><a class="" href="<?php echo URL ?>/home/index">DKLN</a></li>
+                        <?php if (!Session::existed('user_id')) { ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo URL ?>/users/register">Register</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo URL ?>/users/login">Login</a>
+                            </li>
+                        <?php } else { ?>
+                            <?php if (Session::existed('user_cart')) { ?>
+                                <li class="nav-item">
+                                    <a class="" href="<?php echo URL ?>/carts/cart">Cart<i class=""></i>
+                                        <span class="badge badge-danger ml-0" style='border-radius:50%'>
+                                            <?php Session::get('user_cart') ?>
+                                        </span>
+                                    </a>
+                                </li>
+                            <?php } ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo URL ?>/carts/orders">My Orders</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo URL ?>/users/profile">Profile</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo URL ?>/users/logout">Logout</a>
+                            </li>
+
+                        <?php }
+
+                        if (Session::existed('email')) {
+                            ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo URL ?>/users/confirm">Confirm</a>
+                            </li>
+                        <?php } ?>
                         <li><a class="nav-link" href="<?php echo URL ?>/carts/orders">Đơn hàng</a></li>
                         <li><a class="nav-link" href="<?php echo URL ?>/users/profile">Hồ sơ</a></li>
                         <li><a class="nav-link" href="<?php echo URL ?>/users/logout">Đăng suất</a></li>
@@ -50,6 +92,81 @@
                     </div>
                 </div>
             </div>  
+            <div class="bot-header">
+                <div class="container">
+                    <div class="search">
+                        <h2>SHOPDKLN</h2>
+                        <form class="" action='<?php echo URL ?>/home/search' method='POST'>
+                            <input class="" type="text" name='search' placeholder="Search">
+                            <button type="submit"><img src="<?php echo URL ?>/img/Search.png"></button>
+                        </form>
+                    </div>  
+                    <ul class="category">
+                        <?php 
+                if($categories){
+                    foreach ($categories as $cat) {?>
+                        <li class="item">
+                            <a href="<?php echo URL ?>/home/proCategory/<?php echo $cat->cat_id ?>"><?php echo $cat->cat_name ?>
+                            </a>
+                            <ul class="sub_item">
+                            <?php 
+                            if($manufactures){
+                                foreach ($manufactures as $man) {?>
+                                <li>
+                                    <a href="<?php echo URL ?>/home/proManufacture/<?php echo $man->man_id ?>"><?php echo $man->man_name ?></a>
+                                </li>
+                                <?php }
+                            }?>
+                            </ul>
+                        </li>
+                    <?php }
+                }?>
+                                <!-- <li class="item" id="item1">
+                                    <a href="#">PC</a>
+                                    <ul class="sub_item">
+                                        <li><a href="#">Apple</a></li>
+                                        <li><a href="#">Dell</a></li>
+                                        <li><a href="#">Acer</a></li>
+                                        <li><a href="#">Asus</a></li>
+                                        <li><a href="#">MSI</a></li>
+                                    </ul>
+                                </li>
+                                <li class="item">
+                                    <a href="#">Laptop</a>
+                                    <ul class="sub_item">
+                                        <li><a href="#">Apple</a></li>
+                                        <li><a href="#">Dell</a></li>
+                                        <li><a href="#">Acer</a></li>
+                                        <li><a href="#">Asus</a></li>
+                                        <li><a href="#">MSI</a></li>
+                                    </ul>
+                                </li>
+                                <li class="item">
+                                    <a href="#">Tab</a>
+                                    <ul class="sub_item">
+                                        <li><a href="#">Apple</a></li>
+                                        <li><a href="#">Samsung</a></li>
+                                        <li><a href="#">Xiaomi</a></li>
+                                        <li><a href="#">Huawei</a></li>
+                                        <li><a href="#">Lenovo</a></li>
+                                    </ul>
+                                </li>
+                                <li class="item">
+                                    <a href="#">Phone</a>
+                                    <ul class="sub_item">
+                                        <li><a href="#">Apple</a></li>
+                                        <li><a href="#">Samsung</a></li>
+                                        <li><a href="#">Xiaomi</a></li>
+                                        <li><a href="#">Huawei</a></li>
+                                        <li><a href="#">Oppo</a></li>
+                                    </ul>
+                                </li>
+                                <li class="item">
+                                    <a href="#">Gear</a>
+                                </li> -->
+                    </ul>
+                </div>    
+            </div>
             <div class = "header-content">
                 <div class = "logo">
                     <a href = "<?php echo URL ?>/home/index">SHOPDKLN</a>
